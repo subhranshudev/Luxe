b9dc1b7639e337d7f0c1af37da706cd344ebded0
subhranshudev <subhranshusekhar2509@gmail.com>
2026-03-26 18:10:57 +0530

    updated the default image link

--- models/listing.js
+++ models/listing.js
@@ -11,8 +11,10 @@ const listingSchema = new Schema({
   },
   image: {
     type: String,
-    default: "default link",
-    set: (v) => (v === "" ? "image can't reach" : v),
+    default:
+      "https://images.unsplash.com/photo-1774112168823-63b5842067a1?q=80&w=1331&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    set: (v) =>
+      v === "" ? "https://images.unsplash.com/photo-1774112168823-63b5842067a1?q=80&w=1331&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" : v,
   },
   price: {
     type: Number,
